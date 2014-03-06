603d7059857aa676e194af518429132af7851e67
Apprentice <apprentice@dbc35.local>
2014-03-05 18:40:38 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smbsite::Application.config.secret_token = 'bbb461f94478e3bb08f57d4f75a27c92a76afb74d43c02e35e795fd2759d2061f0617acb976cc72eb96ea9ad8602ce0b8d3c8c5fa70977943cb060bc42b80cf2'
